44394a7949affedbf9a729c63024dec045feb929
moraramel <moraramellisa@gmail.com>
2023-09-15 15:31:05 +0300

    task 2 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,8 @@
 fun main(args: Array<String>) {
-    //task 4
-    //Print on the screen and prompts
-    //the user to enter a number
-    print("Enter a number: ")
+    //task 2 3
+    println("What is your name?")
+    val name = readln()
 
-    //read line from standard input - keyboard
-    //and !! operator ensures the input is not null
-    val stringInput = readLine()!!
+    println("Hello, $name")
 
-    //converts the string input to integer
-    var integer:Int = stringInput.toInt()99
-
-    //println() prints the following lineto the output screen
-    println("You entered: $integer")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
